d33413022e6a43b72ac8ffead25b5a294c73816f
Ahid_Naif <ahedqaood19@gmail.com>
2024-05-16 01:10:26 +0300

    update

---
+++ scanner.py
@@ -0,0 +1,19 @@
+import serial
+
+# Open serial port
+ser = serial.Serial('/dev/ttyUSB0', 9600)  # Change '/dev/ttyUSB0' to your serial port
+
+# Send command to request data
+ser.write(b'READ_DATA\n')
+
+# Read response
+response = ser.readline()
+
+# Parse response (assuming response is in ASCII format)
+data = response.decode().strip()
+
+# Close serial port
+ser.close()
+
+# Process the data as needed
+print("Data from GM66 sensor:", data)
