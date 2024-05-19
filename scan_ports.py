cf6ed56ea90a855fd11672ee8e0af97aa15d36ce
Ahid_Naif <ahedqaood19@gmail.com>
2024-05-19 23:56:59 +0300

    update

--- scan_ports.py
+++ scan_ports.py
@@ -1,2 +1,8 @@
 import serial.tools.list_ports
-print(list(serial.tools.list_ports.comports()))
+
+# Get a list of available serial ports
+ports = serial.tools.list_ports.comports()
+
+# Print information about each port
+for port in ports:
+    print(port)
