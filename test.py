1ae08c67e371c56d422d8e8ca0d16d6b2a896125
Ahid_Naif <ahedqaood19@gmail.com>
2024-05-20 02:42:42 +0300

    update

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from huskylib import HuskyLensLibrary
+import json
+from time import sleep
+from escpos import *
+import RPi.GPIO as GPIO          
+import sys
 
 huskyLens = HuskyLensLibrary("I2C","",address=0x32)
 huskyLens.algorthim("ALGORITHM_OBJECT_RECOGNITION")
