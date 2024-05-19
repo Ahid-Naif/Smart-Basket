25c6ecd95c7a555b5cbb52542270e1488bb597f8
Ahid_Naif <ahedqaood19@gmail.com>
2024-05-19 22:01:26 +0300

    add a test code

---
+++ trackTest.py
@@ -0,0 +1,16 @@
+from huskylib import HuskyLensLibrary
+import json
+from time import sleep
+from escpos import *
+import RPi.GPIO as GPIO          
+import sys
+
+huskyLens = HuskyLensLibrary("I2C","",address=0x32)
+huskyLens.algorthim("ALGORITHM_OBJECT_TRACKING")
+while(true):
+    data=huskyLens.blocks()
+    print(data)
+    x=0
+    # for i in data:
+    #     x=x+1
+    #     print("Face {} data: {}".format(x,i)
