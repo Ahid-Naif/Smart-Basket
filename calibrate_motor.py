c93049e02675a7550117672b76db5750be42334b
Ahid_Naif <ahedqaood19@gmail.com>
2024-05-20 23:24:35 +0300

    update

--- calibrate_motor.py
+++ calibrate_motor.py
@@ -1,13 +1,17 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-in1 = 15
-in2 = 14
-en1 = 18
+in1 = 24
+in2 = 23
+en1 = 25
 
-in3 = 24
-in4 = 23
-en2 = 25
+# in1 = 15
+# in2 = 14
+# en1 = 18
+
+# in3 = 24
+# in4 = 23
+# en2 = 25
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
