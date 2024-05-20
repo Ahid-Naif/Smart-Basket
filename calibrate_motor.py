4cb9340e0a66486ac36788c2302feb160248ce10
Ahid_Naif <ahedqaood19@gmail.com>
2024-05-20 23:22:53 +0300

    update

--- calibrate_motor.py
+++ calibrate_motor.py
@@ -16,20 +16,20 @@ GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(en1,GPIO.OUT)
 
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en2,GPIO.OUT)
+# GPIO.setup(in3,GPIO.OUT)
+# GPIO.setup(in4,GPIO.OUT)
+# GPIO.setup(en2,GPIO.OUT)
 
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
 p=GPIO.PWM(en1,1000)
 
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-p2=GPIO.PWM(en2, 1000)
+# GPIO.output(in3,GPIO.LOW)
+# GPIO.output(in4,GPIO.LOW)
+# p2=GPIO.PWM(en2, 1000)
 
 p.start(25)
-p2.start(25)
+# p2.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -45,29 +45,29 @@ while(1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
          print("forward")
-         GPIO.output(in3,GPIO.HIGH)
-         GPIO.output(in4,GPIO.LOW)
+        #  GPIO.output(in3,GPIO.HIGH)
+        #  GPIO.output(in4,GPIO.LOW)
          x='z'
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
          print("backward")
-         GPIO.output(in3,GPIO.LOW)
-         GPIO.output(in4,GPIO.HIGH)
+        #  GPIO.output(in3,GPIO.LOW)
+        #  GPIO.output(in4,GPIO.HIGH)
          x='z'
 
 
     elif x=='s':
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
+        # GPIO.output(in3,GPIO.LOW)
+        # GPIO.output(in4,GPIO.LOW)
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
         x='z'
 
     elif x=='f':
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in4,GPIO.LOW)
+        # GPIO.output(in3,GPIO.HIGH)
+        # GPIO.output(in4,GPIO.LOW)
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
@@ -75,8 +75,8 @@ while(1):
         x='z'
 
     elif x=='b':
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.HIGH)
+        # GPIO.output(in3,GPIO.LOW)
+        # GPIO.output(in4,GPIO.HIGH)
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
@@ -86,26 +86,26 @@ while(1):
     elif x=='l':
         print("low")
         p.ChangeDutyCycle(30)
-        p2.ChangeDutyCycle(30)
+        # p2.ChangeDutyCycle(30)
         x='z'
 
     elif x=='m':
         print("medium")
         p.ChangeDutyCycle(50)
-        p2.ChangeDutyCycle(50)
+        # p2.ChangeDutyCycle(50)
         x='z'
 
     elif x=='h':
         print("high")
         p.ChangeDutyCycle(75)
-        p2.ChangeDutyCycle(75)
+        # p2.ChangeDutyCycle(75)
         x='z'
 
     elif x == 'c':
         print("custom")
         duty_cycle = float(input("Enter duty cycle (0-100): "))
         p.ChangeDutyCycle(duty_cycle)
-        p2.ChangeDutyCycle(duty_cycle)
+        # p2.ChangeDutyCycle(duty_cycle)
      
     elif x=='e':
         GPIO.cleanup()
